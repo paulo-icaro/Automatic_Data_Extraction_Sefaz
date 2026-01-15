726233bcf5cd74d6875e584ed97a5d61a56d1b43
Paulo Icaro <pauloicaro38@gmail.com>
2026-01-15 08:51:32 -0300

    Ajustes para transformações envolvendo média

--- Frequency_Transforming.R
+++ Frequency_Transforming.R
@@ -8,21 +8,21 @@
 # ================= #
 # === Libraries === #
 # ================= #
-library(dplyr)
-library(lubridate)
+tryCatch(expr = suppressWarnings(library(dplyr)),
+         error = function(e){stop('Não é possível prosseguir. Instale os pacotes dplyr.')})
+tryCatch(expr = suppressWarnings(library(lubridate)),
+         error = function(e){stop('Não é possível prosseguir. Instale o pacotes lubridate.')})
+
 
 
 # ============================ #
 # === Transforming Dataset === #
 # ============================ #
 cumulative_transform = function(transform_type, frequency, dataset){
-  #stopifnot('Argumento "data" inválido !' = is.data.frame(dataset))
-  #ncol = ncol(dataset)
-  min_date = min(dataset[[1]])
-  max_date = max(dataset[[1]])
   
-  
+  # ---------------------------- #
   # --- Transform Type - Sum --- #
+  # ---------------------------- #
   if(transform_type == 'soma'){
     if(frequency == 'mensal'){
         dataset = 
@@ -87,7 +87,9 @@ cumulative_transform = function(transform_type, frequency, dataset){
                        .default = 'S1')))
     }
     
+  # ------------------------------------ #  
   # --- Transform Type - Last Period --- #
+  # ------------------------------------ #
   } else if(transform_type == 'periodo_final'){
     if(frequency == 'mensal'){
       dataset = 
@@ -145,6 +147,50 @@ cumulative_transform = function(transform_type, frequency, dataset){
                          month(data) - 6 >= 1 ~ 'S2', 
                          .default = 'S1')))
     }
+    
+  # ----------------------------- #  
+  # --- Transform Type - Mean --- #
+  # ----------------------------- #
+  } else if(transform_type == 'media'){
+    if(frequency == 'mensal'){
+      dataset = unique(
+        dataset %>% 
+        mutate(data = floor_date(x = data, unit = 'month')) %>%
+        group_by(data) %>%
+        mutate(across(colnames(dataset[c(-1)]), mean)) %>%
+        mutate(data = 
+                 paste0(
+                   year(data), 
+                   case_when(
+                     month(data) == 1 ~ 'M1',
+                     month(data) == 2 ~ 'M2',
+                     month(data) == 3 ~ 'M3',
+                     month(data) == 4 ~ 'M4',
+                     month(data) == 5 ~ 'M5',
+                     month(data) == 6 ~ 'M6',
+                     month(data) == 7 ~ 'M7',
+                     month(data) == 8 ~ 'M8',
+                     month(data) == 9 ~ 'M9',
+                     month(data) == 10 ~ 'M10',
+                     month(data) == 11 ~ 'M11',
+                     .default = 'M12'))))
+    } else if(frequency == 'bimestral'){
+      dataset = unique(
+        dataset %>% 
+        mutate(data = floor_date(x = data, unit = 'bimonth')) %>%
+        group_by(data) %>%
+        mutate(across(colnames(dataset[c(-1)]), mean)) %>%
+        mutate(data = 
+                 paste0(
+                   year(data), 
+                   case_when(
+                     month(data) %in% c(1,2) ~ 'B1', 
+                     month(data) %in% c(3,4) ~ 'B2',
+                     month(data) %in% c(5,6) ~ 'B3',
+                     month(data) %in% c(7,8) ~ 'B4',
+                     month(data) %in% c(9,10) ~ 'B5',
+                     .default = 'B6'))))
+    } #else if()
   }
 }
 
