47323a795d9a0f5f5a9c1919e105f7f8f43114a2
Paulo Icaro <pauloicaro38@gmail.com>
2026-02-10 18:59:50 -0300

    Etapa de Seleção de Dados

--- Siconfi_Dataset.R
+++ Siconfi_Dataset.R
@@ -40,25 +40,65 @@ siconfi_dataset = query_rreo(ano, bimestre, tipo_demonstrativo, relatorio, 'E', 
 # === Seleção de Dados === #
 # ======================== #
 
+# --------------------------- #
+# --- Filtragens Iniciais --- #
+# --------------------------- #
+
+# Resultado Previdenciário #
+resultado_previdenciario_bruto = siconfi_dataset[c(1, 3, 9, 12, 13, 14, 15)] %>%
+  filter(str_detect(conta, 'RESULTADO PREVIDENCIÁRIO'), str_detect(anexo, '04'))
+
+# Dívida Consolidada Líquida #
+divida_consolidada_liquida_bruto = siconfi_dataset[c(1, 3, 9, 12, 13, 14, 15)] %>%
+  filter(str_detect(conta, 'DÍVIDA CONSOLIDADA LÍQUIDA'), str_detect(anexo, '06'))
+
+# Despesa Corrente #
+despesa_corrente_bruto = siconfi_dataset[c(1, 3, 9, 12, 13, 14, 15)] %>%
+  filter(str_detect(conta, 'DESPESAS CORRENTES'), str_detect(anexo, '01'))
+
+# Resultado Primário #
+resultado_primario_bruto = siconfi_dataset[c(1, 3, 9, 12, 13, 14, 15)] %>%
+  filter(str_detect(conta, 'RESULTADO PRIMÁRIO'), str_detect(anexo, '06'))
+
+
+# ---------------------------- #
+# --- Filtragens Avançadas --- #
+# ---------------------------- #
+
 # Resultado Previdenciário Pago (Acumulado do Bimestre)
 resultado_previdenciario = siconfi_dataset[c(1, 3, 9, 12, 13, 14, 15)] %>%
-  filter(conta == 'RESULTADO PREVIDENCIÁRIO - FUNDO EM REPARTIÇÃO (XI) = (IX ¿ X)',
-         coluna == 'DESPESAS PAGAS ATÉ O BIMESTRE (f)')
+  filter(conta %in% c('RESULTADO PREVIDENCIÁRIO (XIV) = (X - XIII)',
+                      'RESULTADO PREVIDENCIÁRIO (XV) = (XI - XIV)',
+                      'RESULTADO PREVIDENCIÁRIO (XI) = (IX - X)',
+                      'RESULTADO PREVIDENCIÁRIO - FUNDO EM REPARTIÇÃO (XI) = (IX ¿ X)'),
+         str_detect(coluna, 'DESPESAS LIQUIDADAS ATÉ O BIMESTRE|DESPESAS PAGAS ATÉ O BIMESTRE (f)'))
   
 # Dívida Consolidada Líquida (Até o Bimestre)
 divida_consolidada_liquida = siconfi_dataset[c(1, 3, 9, 12, 13, 14, 15)] %>%
-  filter(conta == 'DÍVIDA CONSOLIDADA LÍQUIDA (XXXI) = (XXVIII - XXIX)' & str_detect(coluna, 'Até o Bimestre'))
+  filter(conta %in% c('DÍVIDA CONSOLIDADA LÍQUIDA (XXXI) = (XXVIII - XXIX)',
+                      'DÍVIDA CONSOLIDADA LÍQUIDA (XLII) = (XXXIX - XL)') & str_detect(coluna, 'Até o Bimestre'))
 
 # Despesa Corrente Paga (Acumulado do Bimestre)
-depesa_corrente = siconfi_dataset[c(1, 3, 9, 12, 13, 14, 15)] %>%
+despesa_corrente = siconfi_dataset[c(1, 3, 9, 12, 13, 14, 15)] %>%
   filter(conta == 'DESPESAS CORRENTES',
          coluna == 'DESPESAS PAGAS ATÉ O BIMESTRE (j)',
          cod_conta == 'DespesasCorrentes')
 
-# Resultado Primário sem RPPS (Acumulado do Bimestre)
-resultado_primario_s_rpps = siconfi_dataset[c(1, 3, 9, 12, 13, 14, 15)] %>%
-  filter(conta == 'RESULTADO PRIMÁRIO (SEM RPPS) - Acima da Linha (XXXV) = (XVIIa - (XXXIIIa +XXXIIIb + XXXIIIc))',
-         coluna == 'VALOR')
+# Resultado Primário (Acumulado no Ano) Obs: sem RPPS a partir de 2023
+resultado_primario = siconfi_dataset[c(1, 3, 9, 12, 13, 14, 15)] %>%
+  filter(conta %in% c('RESULTADO PRIMÁRIO (XIX) = (VII - XVIII)',
+                       'RESULTADO PRIMÁRIO (SEM RPPS) - Abaixo da Linha (LI) = (L) - (XXXVI - XXXVII)',
+                       'RESULTADO PRIMÁRIO (SEM RPPS) - Acima da Linha (XXXV) = (XVIIa - (XXXIIIa +XXXIIIb + XXXIIIc))',
+                       'RESULTADO PRIMÁRIO - Abaixo da Linha (XXXVII) =  XXXVI - (XXV - XXVI)',
+                       'RESULTADO PRIMÁRIO - Abaixo da Linha (XXXIX) =  XXXVIII - (XXV - XXVI)',
+                       'RESULTADO PRIMÁRIO - Acima da Linha (XXIV) = (XIIa - (XXIIIa +XXIIIb + XXIIIc))'),
+         str_detect(coluna, regex('VALOR|VALOR INCORRIDO|Até o Bimestre|DESPESAS LIQUIDADAS ATÉ O BIMESTRE|Despesas Liquidadas até o Bimestre')))
+  # mutate('valor_bimestre' = valor - lag(valor))
+
+
+for(i in seq_along(resultado_primario_s_rpps)){
+  if(resultado_primario_s_rpps$periodo[i] == 1){resultado_primario_s_rpps$valor_bimestre[i] = resultado_primario_s_rpps$valor[i]}
+}
 
 
 
