0e29bfe5ce1395581fbad5660438dc77cdb079d1
Paulo <pauloicaro38@gmail.com>
2026-04-03 20:56:12 -0300

    Versão estável das rotinas de refinamento dos dados

--- Siconfi_Dataset.R
+++ Siconfi_Dataset.R
@@ -5,11 +5,13 @@
 # --- Script by Paulo Icaro --- #
 
 
-# =================== #
-# === Bibliotecas === #
-# =================== #
+# ================= #
+# === Libraries === #
+# ================= #
 library(dplyr)
 library(stringr)
+library(readxl)
+library(openxlsx)
 source('https://raw.githubusercontent.com/paulo-icaro/Siconfi_API/refs/heads/main/Query_RREO.R')
 
 # Obs: Importação manual, caso a linha acima não funcione. Lembre-se de trocar para o seu diretório.
@@ -19,90 +21,135 @@ source('https://raw.githubusercontent.com/paulo-icaro/Siconfi_API/refs/heads/mai
 
 
 
-# ===================================== #
-# === Extração do Conjunto de Dados === #
-# ===================================== #
+# ======================= #
+# === Data Extraction === #
+# ======================= #
 
-# --- Informações Prévias --- #
+# --- Previous Info --- #
 ano = 2015:2025
 bimestre = 1:6
-ente = 23
-relatorio = c('01', '04', '06')
-esfera = 'M'
 tipo_demonstrativo = 'RREO'
+state_municipalities = read_excel(path = 'Databases/Inputs/Lista_Municipios_Brasil.xlsx', sheet = 'Ceara')
 
-# --- Extração --- #
-siconfi_dataset = query_rreo(ano, bimestre, tipo_demonstrativo, relatorio, 'E', ente, TRUE)
-
+# --- Extraction --- #
+siconfi_dataset_est = query_rreo(ano, bimestre, tipo_demonstrativo, c('01', '04', '06'), 'E', 23, TRUE)
+siconfi_dataset_mun = query_rreo(2025, bimestre, tipo_demonstrativo, '01', 'M', state_municipalities$`Cod. 7 Digitos`, TRUE)
 
 
 # ======================== #
-# === Seleção de Dados === #
+# === Data Selection === #
 # ======================== #
 
-# --------------------------- #
-# --- Filtragens Iniciais --- #
-# --------------------------- #
+# ----------------------- #
+# --- Major Filtering --- #
+# ----------------------- #
 
-# Resultado Previdenciário #
-resultado_previdenciario_bruto = siconfi_dataset[c(1, 3, 9, 12, 13, 14, 15)] %>%
+# --- Resultado Previdenciário --- #
+resultado_previdenciario_bruto = siconfi_dataset_est[c(1, 3, 9, 12, 13, 14, 15)] %>%
   filter(str_detect(conta, 'RESULTADO PREVIDENCIÁRIO'), str_detect(anexo, '04'))
 
-# Dívida Consolidada Líquida #
-divida_consolidada_liquida_bruto = siconfi_dataset[c(1, 3, 9, 12, 13, 14, 15)] %>%
+# --- Dívida Consolidada Líquida --- #
+divida_consolidada_liquida_bruto = siconfi_dataset_est[c(1, 3, 9, 12, 13, 14, 15)] %>%
   filter(str_detect(conta, 'DÍVIDA CONSOLIDADA LÍQUIDA'), str_detect(anexo, '06'))
 
-# Despesa Corrente #
-despesa_corrente_bruto = siconfi_dataset[c(1, 3, 9, 12, 13, 14, 15)] %>%
-  filter(str_detect(conta, 'DESPESAS CORRENTES'), str_detect(anexo, '01'))
-
-# Resultado Primário #
-resultado_primario_bruto = siconfi_dataset[c(1, 3, 9, 12, 13, 14, 15)] %>%
+# --- Resultado Primário --- #
+resultado_primario_bruto = siconfi_dataset_est[c(1, 3, 9, 12, 13, 14, 15)] %>%
   filter(str_detect(conta, 'RESULTADO PRIMÁRIO'), str_detect(anexo, '06'))
 
+# --- Investimento Municipal --- #
+investimento_municipal_bruto = siconfi_dataset_mun[c(1, 3, 5, 9, 12, 13, 14, 15)] %>%
+  filter(str_detect(conta, 'INVESTIMENTOS'), str_detect(anexo, '01'))
 
-# ---------------------------- #
-# --- Filtragens Avançadas --- #
-# ---------------------------- #
+# Despesa Corrente #
+# despesa_corrente_bruto = siconfi_dataset_est[c(1, 3, 9, 12, 13, 14, 15)] %>%
+#   filter(str_detect(conta, 'DESPESAS CORRENTES'), str_detect(anexo, '01'))
 
-# Resultado Previdenciário Pago (Acumulado do Bimestre)
-resultado_previdenciario = siconfi_dataset[c(1, 3, 9, 12, 13, 14, 15)] %>%
-  filter(conta %in% c('RESULTADO PREVIDENCIÁRIO (XIV) = (X - XIII)',
-                      'RESULTADO PREVIDENCIÁRIO (XV) = (XI - XIV)',
-                      'RESULTADO PREVIDENCIÁRIO (XI) = (IX - X)',
-                      'RESULTADO PREVIDENCIÁRIO - FUNDO EM REPARTIÇÃO (XI) = (IX ¿ X)'),
-         str_detect(coluna, 'DESPESAS LIQUIDADAS ATÉ O BIMESTRE|DESPESAS PAGAS ATÉ O BIMESTRE (f)'))
+
+
+# ----------------------- #
+# --- Minor Filtering --- #
+# ----------------------- #
+
+# --- Resultado Previdenciário Pago (Acumulado do Bimestre) --- #
+resultado_previdenciario =
+  resultado_previdenciario_bruto %>%
+  filter(conta %in% c('RESULTADO PREVIDENCIÁRIO - FUNDO EM REPARTIÇÃO (XI) = (IX ¿ X)'),
+         str_detect(coluna, 'DESPESAS PAGAS ATÉ O BIMESTRE')) %>%
+  mutate(data = paste0(exercicio, 'B', periodo)) %>%
+  mutate('res_prev' = ifelse(periodo == 1, valor, valor - lag(valor)))
+
   
-# Dívida Consolidada Líquida (Até o Bimestre)
-divida_consolidada_liquida = siconfi_dataset[c(1, 3, 9, 12, 13, 14, 15)] %>%
+# --- Dívida Consolidada Líquida (Até o Bimestre) --- #
+divida_consolidada_liquida =
+  divida_consolidada_liquida_bruto %>%
   filter(conta %in% c('DÍVIDA CONSOLIDADA LÍQUIDA (XXXI) = (XXVIII - XXIX)',
-                      'DÍVIDA CONSOLIDADA LÍQUIDA (XLII) = (XXXIX - XL)') & str_detect(coluna, 'Até o Bimestre'))
+                      'DÍVIDA CONSOLIDADA LÍQUIDA (XLII) = (XXXIX - XL)') & str_detect(coluna, 'Até o Bimestre')) %>%
+  mutate(data = paste0(exercicio, 'B', periodo))
+
+colnames(divida_consolidada_liquida)[7] = 'div_cons'
+
+
+
+# --- Resultado Primário (Acumulado no Ano) --- #
+resultado_primario =
+  resultado_primario_bruto %>%
+  filter(conta %in% c('RESULTADO PRIMÁRIO (SEM RPPS) - Acima da Linha (XXXV) = (XVIIa - (XXXIIIa +XXXIIIb + XXXIIIc))'),
+         str_detect(coluna, regex('VALOR'))) %>%
+  mutate(data = paste0(exercicio, 'B', periodo)) %>%
+  mutate('res_prim' = ifelse(periodo == 1, valor, valor - lag(valor)))
+
+# --- Investimento Municipal --- # 
+investimento_municipal =
+  investimento_municipal_bruto %>%
+  filter(conta %in% c('INVESTIMENTOS'),
+         str_detect(coluna, 'DESPESAS PAGAS ATÉ O BIMESTRE')) %>%
+  mutate(data = paste0(exercicio, 'B', periodo)) %>%
+  group_by(data) %>%
+  select(data, exercicio, periodo, valor) %>%
+  summarize('inv_mun_acum' = sum(valor)) %>%
+  mutate('inv_mun' = ifelse(substr(data, 6, 6) == 1, inv_mun_acum, inv_mun_acum - lag(inv_mun_acum)))
+  
 
 # Despesa Corrente Paga (Acumulado do Bimestre)
-despesa_corrente = siconfi_dataset[c(1, 3, 9, 12, 13, 14, 15)] %>%
-  filter(conta == 'DESPESAS CORRENTES',
-         coluna == 'DESPESAS PAGAS ATÉ O BIMESTRE (j)',
-         cod_conta == 'DespesasCorrentes')
-
-# Resultado Primário (Acumulado no Ano) Obs: sem RPPS a partir de 2023
-resultado_primario = siconfi_dataset[c(1, 3, 9, 12, 13, 14, 15)] %>%
-  filter(conta %in% c('RESULTADO PRIMÁRIO (XIX) = (VII - XVIII)',
-                       'RESULTADO PRIMÁRIO (SEM RPPS) - Abaixo da Linha (LI) = (L) - (XXXVI - XXXVII)',
-                       'RESULTADO PRIMÁRIO (SEM RPPS) - Acima da Linha (XXXV) = (XVIIa - (XXXIIIa +XXXIIIb + XXXIIIc))',
-                       'RESULTADO PRIMÁRIO - Abaixo da Linha (XXXVII) =  XXXVI - (XXV - XXVI)',
-                       'RESULTADO PRIMÁRIO - Abaixo da Linha (XXXIX) =  XXXVIII - (XXV - XXVI)',
-                       'RESULTADO PRIMÁRIO - Acima da Linha (XXIV) = (XIIa - (XXIIIa +XXIIIb + XXIIIc))'),
-         str_detect(coluna, regex('VALOR|VALOR INCORRIDO|Até o Bimestre|DESPESAS LIQUIDADAS ATÉ O BIMESTRE|Despesas Liquidadas até o Bimestre')))
-  # mutate('valor_bimestre' = valor - lag(valor))
+# despesa_corrente = siconfi_dataset[c(1, 3, 9, 12, 13, 14, 15)] %>%
+#   filter(conta == 'DESPESAS CORRENTES',
+#          coluna == 'DESPESAS PAGAS ATÉ O BIMESTRE (j)',
+#          cod_conta == 'DespesasCorrentes')
 
 
-for(i in seq_along(resultado_primario_s_rpps)){
-  if(resultado_primario_s_rpps$periodo[i] == 1){resultado_primario_s_rpps$valor_bimestre[i] = resultado_primario_s_rpps$valor[i]}
+
+
+# ========================= #
+# === Joining Variables === #
+# ========================= #
+
+# Obs: Adjust according to the largest dataframe
+siconfi_macro_bimonthly = 
+  divida_consolidada_liquida[c('data', 'div_cons')] %>%
+  left_join(y = resultado_previdenciario[c('data', 'res_prev')], by = 'data') %>%
+  left_join(y = resultado_primario[c('data', 'res_prim')], by = 'data') %>%
+  left_join(y = investimento_municipal[c('data', 'inv_mun')], by = 'data') %>%
+  mutate(res_prim_prev = res_prev + res_prim)
+
+
+
+# ======================= #
+# === Storing Results === #
+# ======================= #
+wb = createWorkbook(creator = 'Sefaz-CE')
+addWorksheet(wb = wb, sheetName = 'tempo')
+addWorksheet(wb = wb, sheetName = 'macro')
+writeData(wb = wb, sheet = 'tempo', x = siconfi_macro_bimonthly %>% select(data), rowNames = FALSE)
+writeData(wb = wb, sheet = 'macro', x = siconfi_macro_bimonthly %>% select(-data), rowNames = FALSE)
+saveWorkbook(wb = wb, file = 'Databases/Outputs/db_siconfi.xlsx', overwrite = TRUE)
+
+
+
+# ================= #
+# === Cleansing === #
+# ================= #
+patterns = c('_bruto', '_dataset')
+for(i in seq_along(patterns)){
+  rm(list = ls(pattern = patterns[i]))
 }
-
-
-
-# =============== #
-# === Limpeza === #
-# =============== #
-rm(ano, bimestre, ente, esfera, relatorio, tipo_demonstrativo)
+rm(patterns, i, ano, bimestre, tipo_demonstrativo)
